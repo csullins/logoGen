45487e99a0374152646e8ebfaed28ff41dc4cd2b
csullins <camilesullins@gmail.com>
2023-04-15 11:49:25 -0500

    edit inquirer prompts

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const genLogo = ({ shape, color, text }) =>
+const genLogo = ({ shape, color, text, textColor }) =>
   inquirer
     .prompt([
       {
         type: "list",
         name: "shape",
         message: "Welcome to Logo Creator! What shape would you like your logo to be?",
-        choices: ['circle', 'rectangle', 'square']
+        choices: ['circle', 'triangle', 'square']
       },
       {
-        type: "list",
+        type: "input",
         name: "color",
         message: `Awesome! What color would you like that ${shape} to be?`,
-        choices: ['black', 'yellow', 'blue', 'orange', 'magenta']
       },
       {
         type: "input",
         name: "text",
-        message: `Excellent choices. One more thing, what text do you want inside the ${color} ${shape}?`,
+        message: `Excellent choices. What text do you want inside the ${color} ${shape}? Enter up to 3 letters.`,
       },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: `One last thing, what color should ${text} display in.`
+      }
     ])
     .then((data) => {
       logoSpecs = genLogo(data);
 
       fs.writeFile("logo.svg", logoSpecs, (err) =>
         err
-          ? console.log(err)
-          : console.log(
-              "Woohoo! Your logo is ready. Open up logo.svg to check it out."
+          ? console.log(err) : console.log(
+              "Woohoo! Your logo is ready. Open up logo.svg in your browser to check it out."
             )
       );
     });
