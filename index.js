8b5ec5e9902b5c841900d37cd8cc2cf0b9ad6ef3
csullins <camilesullins@gmail.com>
2023-04-16 16:13:52 -0500

    got index.js working from cli

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
+const CLI = require('./lib/cli');
 
-const genLogo = ({ shape, color, text, textColor }) =>
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "shape",
-        message: "Welcome to Logo Creator! What shape would you like your logo to be?",
-        choices: ['circle', 'triangle', 'square']
-      },
-      {
-        type: "input",
-        name: "color",
-        message: `Awesome! What color would you like that ${shape} to be?`,
-      },
-      {
-        type: "input",
-        name: "text",
-        message: `Excellent choices. What text do you want inside the ${color} ${shape}? Enter up to 3 letters.`,
-      },
-      {
-        type: 'input',
-        name: 'textColor',
-        message: `One last thing, what color should ${text} display in.`
-      }
-    ])
-    .then((data) => {
-      logoSpecs = genLogo(data);
+const cli = new CLI();
 
-      fs.writeFile("logo.svg", logoSpecs, (err) =>
-        err
-          ? console.log(err) : console.log(
-              "Woohoo! Your logo is ready. Open up logo.svg in your browser to check it out."
-            )
-      );
-    });
+cli.run();
+
